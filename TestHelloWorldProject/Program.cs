46e766b8510fc68617343075bf550c2a5c01f778
Ashish <ashish2000patil@gmail.com>
2022-08-29 20:17:31 +0530

    Object and Class Related

---
+++ TestHelloWorldProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloWorldProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Dog dog1 = new Dog();
+            dog1.color = "brown";
+            dog1.name = "Tommy";
+            dog1.breed = "Labour";
+
+            Console.WriteLine(dog1.color);
+            dog1.dogdetails();
+
+        }
+    }
+}

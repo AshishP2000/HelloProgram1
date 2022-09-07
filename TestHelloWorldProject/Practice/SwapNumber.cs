aee6e53be2b4c2ac4c245ddd1c34ff9f524ebe5a
Ashish <ashish2000patil@gmail.com>
2022-09-07 21:51:16 +0530

    [Ashish]Added: Swap Numbers

---
+++ TestHelloWorldProject/Practice/SwapNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloWorldProject.Practice
+{
+    public class SwapNumber
+    {
+        public static void Swap(int num1,int num2)
+        {
+            int num3;
+            Console.WriteLine("Before Swapping");
+            Console.WriteLine("Number 1: {0}, Number 2: {1}", num1, num2);
+            
+            num3 = num1;
+            num1 = num2;
+            num2 = num3;
+            Console.WriteLine("After Swapping");
+            Console.WriteLine("Number 1: {0}, Number 2: {1}", num1,num2);
+        }
+    }
+}

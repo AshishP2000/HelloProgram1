add41eebb23de4a701a3ab352044d3a14c21f544
Ashish <ashish2000patil@gmail.com>
2022-09-07 22:28:47 +0530

    [Ashish]Added: Maximum and Minimum

---
+++ TestHelloWorldProject/Practice/MaxMin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloWorldProject.Practice
+{
+    public class MaxMin
+    {
+        public static void Maxmin(int num1,int num2,int num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("num1({0}) is greater than num2({1}) and num3({2})", num1, num2, num3);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("num2({0}) is greater than num1({1}) and num3({2})", num2, num1, num3);
+            }
+            else 
+            {
+                Console.WriteLine("num3({0}) is greater than num1({1}) and num2({2})", num3, num1, num2);
+            }
+        }
+    }
+}

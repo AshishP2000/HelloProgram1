310423ec2ac904f67f9a0c6bc02404bb35084aea
Ashish <ashish2000patil@gmail.com>
2022-09-07 22:19:57 +0530

    [Ashish]Added: Even Odd

---
+++ TestHelloWorldProject/Practice/EvenOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloWorldProject.Practice
+{
+    public class EvenOdd
+    {
+        public static void Evenodd(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Given Number is Even");
+            }
+            else {
+                Console.WriteLine("Given Number is Odd");
+            }
+        }
+    }
+}

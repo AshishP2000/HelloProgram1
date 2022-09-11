cce8517178f4adc24fe9faa8495204a98328545f
Ashish <ashish2000patil@gmail.com>
2022-09-11 09:25:37 +0530

    [Ashish]Added: Abstraction

---
+++ TestHelloWorldProject/OOPsConcepts/Abstraction/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloWorldProject.OOPsConcepts.Abstraction
+{
+    public abstract class Shape
+    {
+        public abstract void Area(double side);
+
+        public void AreaDetails()
+        {
+            Console.WriteLine("Are Details:");
+        }
+        
+    }
+}
---
+++ TestHelloWorldProject/OOPsConcepts/Abstraction/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelloWorldProject.OOPsConcepts.Abstraction
+{
+    public class Square : Shape
+    {
+        
+        public override void Area(double side)
+        {
+            double area = side * side;
+            Console.WriteLine("Area of Square is: {0}",area);
+        }
+    }
+}
